2beaa96c494e1d3f5e98b7275dbcb97f3c43c8e6
SEOYEONHA <only3322@naver.com>
2023-12-05 11:20:31 +0900

    코드 설명 추가

--- day02/src/welcome/HelloWorld.java
+++ day02/src/welcome/HelloWorld.java
@@ -1,8 +1,9 @@
 package welcome;
 
-public class HelloWorld {
+public class HelloWorld {//파일을 대표하는 영역
 	public static void main(String[]args) {
-		System.out.println("Hello Java");
+		System.out.println("Hello Java!");
 		//System.out.println("안녕 자바");
 	}
 }
+
