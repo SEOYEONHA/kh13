0b8327b1bce71d2ee969cbb83eb64748d91d2ee3
SEOYEONHA <only3322@naver.com>
2023-12-06 13:24:28 +0900

    주차요금

--- day02/src/data/Test07주차장2.java
+++ day02/src/data/Test07주차장2.java
@@ -9,12 +9,13 @@ public class Test07주차장2 {
 		//11시 50분에 들어와서 17시 25분에 나간 차량의 요금을 출력
 		//(단, 다음날까지 주차는 절대로 불가능합니다)
 		
+		//입력
 		int Price = 1500;
 		int payTime = 10;
 		int inHour = 11 , inMinute = 50;
 		int outHour = 17 , outMinute = 25;
 		
-		
+		//계산
 		int inTIME = inHour * 60 + inMinute , outTime = outHour * 60 + outMinute;
 		int parkTime = outTime - inTIME; 
 		int hour = parkTime / 60;
@@ -22,6 +23,7 @@ public class Test07주차장2 {
 		//System.out.println(parkTime); //총 주차 시간(분)
 		int totalPrice = Price * (parkTime / payTime);
 
+		//출력
 		System.out.println(hour);
 		System.out.println(minute);
 		System.out.println(totalPrice);
