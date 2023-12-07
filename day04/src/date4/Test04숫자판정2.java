9e1eaaf317fbfebea5c23d7813d0d6c6ed5f441c
SEOYEONHA <only3322@naver.com>
2023-12-07 15:19:51 +0900

    7이 들어간 숫자 판정 프로그램(강사님 ver)

---
+++ day04/src/date4/Test04숫자판정2.java
@@ -0,0 +1,18 @@
+package date4;
+
+public class Test04숫자판정2 {
+	public static void main(String[] args) {
+		//어떤 두 자리 숫자가 주어졌을 때
+		//이 숫자에 7이 포함되어 있는지 검사하여 결과 출력
+		
+		//int number = 71;
+
+		//위와 같은 상황이라면 true가 나와야 합니다.
+		//참고로 자바에서 같다는 ==를 사용합니다
+		
+		//입력
+	
+	}
+	
+
+}
