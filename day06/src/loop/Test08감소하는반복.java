939a7808e5bdff1d9a19a5c758a40f4fec53aff0
SEOYEONHA <only3322@naver.com>
2023-12-11 17:04:19 +0900

    감소하는 반복문 예제

---
+++ day06/src/loop/Test08감소하는반복.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Test08감소하는반복 {
+	public static void main(String[] args) {
+		//(Q) 10부터 0까지 줄어드는 카운트다운 출력
+		// - 범위조심
+		// - 증가가 아닌 감소처리를 해야함
+		
+		for(int i = 10 ; i >= 0 ; i --) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
