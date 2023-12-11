4414a1c50b821ff72f7cf679b321860165656d6e
SEOYEONHA <only3322@naver.com>
2023-12-11 15:59:40 +0900

    필터링 예제 문제풀이

---
+++ day06/src/loop/Test05필터링.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class Test05필터링 {
+	public static void main(String[] args) {
+		//Test05필터링
+		//다음 숫자를 화면에 출력해보세요
+		
+		//1부터 20 사이의 홀수
+		//1부터 50 사이의 3의배수
+		
+		for(int i = 1 ; i <= 19 ; i = i + 2) {
+			System.out.println(i);
+		}
+		
+		for(int i = 3 ; i <= 48 ; i = i + 3 ) {
+			System.out.println(i);
+			}
+		}
+}
+
