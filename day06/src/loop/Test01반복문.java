099e925682991aa7c67644a0d1f8925a4a0c1972
SEOYEONHA <only3322@naver.com>
2023-12-11 13:39:39 +0900

    반복문 예제

---
+++ day06/src/loop/Test01반복문.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class Test01반복문 {
+	public static void main(String[] args) {
+		//(Q) 화면에 Hello Java를 5번 출력하세요
+		
+		for(int i = 1 ; i <= 10 ; i = i + 1) {
+			System.out.println("Hello Java");
+		}
+	}
+
+}
