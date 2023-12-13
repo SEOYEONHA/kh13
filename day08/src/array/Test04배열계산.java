39d89e94aeb74ec114fa875de69fa2705ae71f94
SEOYEONHA <only3322@naver.com>
2023-12-13 18:06:28 +0900

    배열계산

--- day08/src/array/Test04배열계산.java
+++ day08/src/array/Test04배열계산.java
@@ -42,12 +42,13 @@ public class Test04배열계산 {
 		System.out.println("평균 : " + (double)total / scoreList.length + "점");
 		
 		int grade = 1;
+		int point = 80;
 		
 		// [4]만약 80점인 학생이 전학을 온다면 예상되는 등수를 출력
 		System.out.println();
 		System.out.print("만약 80점인 학생이 전학을 온다면 예상되는 등수 : ");
 		for(int i = 0 ; i < scoreList.length ; i++) {
-			if(scoreList[i] > 80) {
+			if(scoreList[i] > point) {
 				grade ++;
 			}
 		}
