4a1afc32ecc2184a2bcefbed34c43bb1a3cceec3
SEOYEONHA <only3322@naver.com>
2023-12-19 13:18:23 +0900

    getter메소드 사용한 문제풀이

---
+++ day12/src/opp/method9/Test01.java
@@ -0,0 +1,19 @@
+package opp.method9;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Bank a = new Bank();
+		Bank b = new Bank();
+		Bank c = new Bank();
+		
+		
+		a.data("유재석", 2.2, 1, 500000, 2);
+		b.data("강호동", 2.5, 1, 500000, 2);
+		c.data("신동엽", 2.35, 1.5, 600000, 3);
+		
+		a.information();
+		b.information();
+		c.information();
+	}
+
+}
