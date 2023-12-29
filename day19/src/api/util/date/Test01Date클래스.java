9885c47f7e5dfb88de3e55d5825ddc08827f9454
SEOYEONHA <only3322@naver.com>
2023-12-29 09:57:11 +0900

    Date 클래스 객체 생성

---
+++ day19/src/api/util/date/Test01Date클래스.java
@@ -0,0 +1,15 @@
+package api.util.date;
+
+import java.util.Date;
+
+public class Test01Date클래스 {
+	public static void main(String[] args) {
+		//Date 클래스
+		
+		Date a = new Date();
+		Date b = new Date(2023, 12, 29);
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
