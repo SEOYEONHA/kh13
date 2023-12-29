674932527cb6b7203c2ec8422d4c5593529b0629
SEOYEONHA <only3322@naver.com>
2023-12-29 09:57:29 +0900

    Date 클래스 객체 생성

--- day19/src/api/util/date/Test01Date클래스.java
+++ day19/src/api/util/date/Test01Date클래스.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public class Test01Date클래스 {
 	public static void main(String[] args) {
 		//Date 클래스
+		//- 밀레니엄 버그(Y2K)로 인해 대부분의 기능이 비추천됨
+		//- 현재 시각을 구하는 기능은 여전히 사용
 		
 		Date a = new Date();
 		Date b = new Date(2023, 12, 29);
