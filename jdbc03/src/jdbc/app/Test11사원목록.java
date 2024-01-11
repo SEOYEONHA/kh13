174f16b83656987aaae5663707211478ab0f0a07
SEOYEONHA <only3322@naver.com>
2024-01-11 09:50:50 +0900

    사원목록

--- jdbc03/src/jdbc/app/Test11사원목록.java
+++ jdbc03/src/jdbc/app/Test11사원목록.java
@@ -13,7 +13,7 @@ public class Test11사원목록 {
 		
 		System.out.println("조회 결과 : 총 " + list.size() + "건");
 		for(EmpDto dto:list) {			
-			System.out.println("사원 이름 : "+dto.getEmpName()+" / 부서명 : "+dto.getEmpDept());
+			System.out.println("사원 이름 : " + dto.getEmpName() + " / 부서명 : " + dto.getEmpDept());
 		}
 	}
 }
