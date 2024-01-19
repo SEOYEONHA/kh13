984262cc0a7b1fc1464e62bc2d8e0a99444457a8
SEOYEONHA <only3322@naver.com>
2024-01-19 15:04:54 +0900

    EL을 이용한 파라미터 처리

--- spring09view/src/main/java/com/kh/spring09/controller/ElJstlController.java
+++ spring09view/src/main/java/com/kh/spring09/controller/ElJstlController.java
@@ -1,5 +1,7 @@
 package com.kh.spring09.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,17 @@ public class ElJstlController {
 		dto.setPocketmonType("전기");
 		model.addAttribute("dto", dto);
 		return "/WEB-INF/views/elJstl/test02.jsp";
+	}
+	
+	@RequestMapping("/test03")
+	public String test03() {
+		return "/WEB-INF/views/elJstl/test03.jsp";
+	}
+	
+	@RequestMapping("/test04")
+	public String test04(Model model) {
+		List<Integer> list = List.of(7, 13, 17, 30, 43, 4);
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/elJstl/test04.jsp";
 	}
 }
