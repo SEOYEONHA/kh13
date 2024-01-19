abc21a84515265e8038450d9ce2b8b8dd12befff
SEOYEONHA <only3322@naver.com>
2024-01-19 13:59:26 +0900

    EL에서의 객체 출력

--- spring09view/src/main/java/com/kh/spring09/controller/ElJstlController.java
+++ spring09view/src/main/java/com/kh/spring09/controller/ElJstlController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.spring09.dto.PocketmonDto;
+
 @Controller
 @RequestMapping("/el-jstl")
 public class ElJstlController {
@@ -16,4 +18,14 @@ public class ElJstlController {
 		return "/WEB-INF/views/elJstl/test01.jsp";
 	}
 
+	@RequestMapping("/test02")
+	public String test02(Model model) {
+		//객체 추가
+		PocketmonDto dto = new PocketmonDto();	
+		dto.setPocketmonNo(21);
+		dto.setPocketmonName("피카츄");
+		dto.setPocketmonType("전기");
+		model.addAttribute("dto", dto);
+		return "/WEB-INF/views/elJstl/test02.jsp";
+	}
 }
