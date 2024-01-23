5f428a98bb4a1e551c517fd769a1834764a1e5f4
SEOYEONHA <only3322@naver.com>
2024-01-23 15:58:47 +0900

    로그인1

--- spring10integrated/src/main/java/com/kh/spring10/controller/MemberController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/MemberController.java
@@ -2,10 +2,12 @@ package com.kh.spring10.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.spring10.dao.MemberDao;
 import com.kh.spring10.dto.MemberDto;
@@ -32,4 +34,28 @@ public class MemberController {
 	public String joinFinish() {
 		return "/WEB-INF/views/member/joinFinish.jsp";
 	}
+	
+	//로그인
+	@GetMapping("/login")
+	public String login() {
+		return "/WEB-INF/views/member/longin.jsp";
+	}
+	@PostMapping("/login")
+	public String login(Model model,
+							@ModelAttribute MemberDto dto,
+							@RequestParam String memberId,
+							@RequestParam String memberPw) {
+		boolean pass = memberId.equals(dto.getMemberId()) && memberPw.equals(dto.getMemberPw());
+		model.addAttribute("dto", dto);
+		model.addAttribute("pass", pass);
+		if(pass) {
+			return "/WEB-INF/views/home";
+		}
+		else {
+			return "/WEB-INF/views/member/login.jsp";
+		}
+	}
+	
+	
+			
 }
