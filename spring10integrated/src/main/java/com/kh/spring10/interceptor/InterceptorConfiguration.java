e0995ea2b678728ce6057168f9c8233382dee1ad
user1 <user1@DESKTOP-FC7GG0L>
2024-01-25 14:03:43 +0900

    .

---
+++ spring10integrated/src/main/java/com/kh/spring10/interceptor/InterceptorConfiguration.java
@@ -0,0 +1,9 @@
+package com.kh.spring10.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+
+//application.properties에서 하기 힘든 설정들을 이곳에서 구현할 수 있다
+@Configuration
+public class InterceptorConfiguration {
+
+}
