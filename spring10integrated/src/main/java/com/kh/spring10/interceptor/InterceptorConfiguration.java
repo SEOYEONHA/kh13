544a2d9b7a5850af7efa89f7936ac2cc00b599b1
user1 <user1@DESKTOP-FC7GG0L>
2024-01-25 14:20:01 +0900

    AOP의 기본개념 및 interceptor 코드

--- spring10integrated/src/main/java/com/kh/spring10/interceptor/InterceptorConfiguration.java
+++ spring10integrated/src/main/java/com/kh/spring10/interceptor/InterceptorConfiguration.java
@@ -1,9 +1,24 @@
 package com.kh.spring10.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //application.properties에서 하기 힘든 설정들을 이곳에서 구현할 수 있다
+//[1] 상속을 통한 자격 획득 (WebMvcConfigurer)
+//[2] 등록
+//[3] 필요한 메소드를 재정의하여 설정 구현
 @Configuration
-public class InterceptorConfiguration {
-
+public class InterceptorConfiguration implements WebMvcConfigurer{
+	@Autowired
+	private TestInterceptor testInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//registry에다가 인터셉터를 주소와 함께 등록
+		//registry.addInterceptor(인터샙터객체).addPathPatterns(주소);
+		//registry.addInterceptor(testInterceptor).addPathPatterns("/**"); // testInterceptor이 아이를 모든주소에 전부다 라는 뜻
+		registry.addInterceptor(testInterceptor).addPathPatterns("/pocketmon/list", "/emp/list");
+	}
 }
--- spring10integrated/src/main/java/com/kh/spring10/interceptor/TestInterceptor.java
+++ spring10integrated/src/main/java/com/kh/spring10/interceptor/TestInterceptor.java
@@ -6,12 +6,16 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+// ** 컨트롤러를 건드리지 않고 앞과 뒤에 뭔가 해보려고 하는것!! (대상을 안건드리기 위해
+//간섭하려고 **
+
 //특정 주소(URL)에 대한 처리 과정을 간섭할 수 있는 객체
 //인터셉터는 특별한 처리를 수행하기 때문에 만드는 방법이 정해져있다
 //[1] 상속을 통해 자격요건을 획득한다
 //[2] 스프링에 등록한다
 //[3] 간섭 메소드를 재정의하여 내용을 작성한다
 // -preHandle 메소드는 컨트롤러가 실행되기 전 시점을 간섭할 수 있다
+//		- true를 반환하면 통과, false를 반환하면 차단
 
 @Service
 public class TestInterceptor implements HandlerInterceptor{
@@ -21,6 +25,7 @@ public class TestInterceptor implements HandlerInterceptor{
 			throws Exception {
 		System.out.println("인터셉터 실행");
 		return true; //통과
+		//return false; //차단(아무것도 설정 안하면 하얀화면으로만 나옴)
 	}
 
 }
