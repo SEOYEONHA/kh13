1f7fcb2f54a52b706d7187ef10b7076d3627a973
SEOYEONHA <88278045+SEOYEONHA@users.noreply.github.com>
2024-01-26 12:44:27 +0900

    Update pocketmon.sql

--- database/pocketmon.sql
+++ database/pocketmon.sql
@@ -12,5 +12,11 @@ insert into pocketmon(pocketmon_no, pocketmon_name, pocketmon_type)
 values(4, '파이리', '불꽃');
 insert into pocketmon(pocketmon_no, pocketmon_name, pocketmon_type)
 values(7, '꼬부기', '물');
+insert into pocketmon(pocketmon_no, pocketmon_name, pocketmon_type)
+values(5, '리자드', '불꽃');
+insert into pocketmon(pocketmon_no, pocketmon_name, pocketmon_type)
+values(9, '거북왕', '물');
+insert into pocketmon(pocketmon_no, pocketmon_name, pocketmon_type)
+values(10, '캐터피', '벌레');
 
 select * from pocketmon;
