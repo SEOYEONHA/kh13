a2ca32f62df3386ca35f010f629c55cfd7646e17
SEOYEONHA <only3322@naver.com>
2024-02-05 17:09:00 +0900

    파일다운로드 2차

--- spring10integrated/src/main/java/com/kh/spring10/controller/FileDownloadController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/FileDownloadController.java
@@ -2,10 +2,14 @@ package com.kh.spring10.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +47,7 @@ public class FileDownloadController {
 		}
 		
 		//3. 실제 파일을 불러온다 (apache commons io, apache commons fileupload)
-		File dir = new File(System.getProperty("user.home"), "upload");
+		File dir = new File(System.getProperty("user.home"), "khUpload");
 		File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
 		
 		byte[] data = FileUtils.readFileToByteArray(target); //파일을 읽어라
@@ -53,11 +57,25 @@ public class FileDownloadController {
 		//- 추가적인 정보를 제공해서 파일 다운로드 처리가 일어나도록 구현
 		//- 추가적인 정보는 header에 설정
 		//return ResponseEntity.status(200).body(내용);
+//		return ResponseEntity.ok()
+//				.header("Content-Encoding", "UTF-8")
+//				.header("Content-Type", attachDto.getAttachType())
+//				.header("Content-Type", "application/octet-stream") //무조건 다운로드
+//				.header("Content-Length", String.valueOf(attachDto.getAttachSize()))
+//				.header("Content-Disposition", "attachment; filename=\"" + attachDto.getAttachName() + "\"")
+//				.body(resource);
+		
 		return ResponseEntity.ok()
-				.header("Content-Encoding", "UTF-8")
-				.header("Content-Type", attachDto.getAttachType())
-				.header("Content-Length", String.valueOf(attachDto.getAttachSize()))
-				.header("Content-Disposition", "attachment; filename=" + attachDto.getAttachName())
+				//.header(HttpHeaders.CONTENT_ENCODING, "UTF-8")
+				.header(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name())
+				//.header(HttpHeaders.CONTENT_TYPE, attachDto.getAttachType())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.contentLength(attachDto.getAttachSize())
+				.header(HttpHeaders.CONTENT_DISPOSITION, 
+						ContentDisposition.attachment()
+									.filename(attachDto.getAttachName(), StandardCharsets.UTF_8)
+									.build().toString()
+				)
 				.body(resource);
 	}
 	
