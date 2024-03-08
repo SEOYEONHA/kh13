13462f4851d0218670be5df9afa6ab30794d5806
SEOYEONHA <only3322@naver.com>
2024-03-08 14:14:08 +0900

    생성된 인증번호 저장

---
+++ spring10integrated/src/main/java/com/kh/spring10/dao/CertDao.java
@@ -0,0 +1,59 @@
+package com.kh.spring10.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring10.dto.CertDto;
+import com.kh.spring10.mapper.CertMapper;
+
+@Repository
+public class CertDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private CertMapper certMapper;
+	
+	//등록
+	public void insert(CertDto certDto) {
+		String sql = "insert into cert(cert_email, cert_number) values(?, ?)";
+		Object[] data = {certDto.getCertEmail(), certDto.getCertNumber()};
+		
+		jdbcTemplate.update(sql, data);
+	}
+	
+	public boolean delete(String certEmail) {
+		String sql = "delete cert where cert_email = ?";
+		Object[] data = {certEmail};
+		
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+	
+	public CertDto selectOne(String certEmail) {
+		String sql = "select * from cer where cert_email = ?";
+		Object[] data = {certEmail};
+		List<CertDto> list = jdbcTemplate.query(sql, certMapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
+	public boolean checkValid(CertDto certDto) {
+		String sql = "select * from cert "
+						+ "where cert_email = ? and cert_number = ? "
+						+ "and cert_time >= sysdate-5/24/60"; //시간은 기본 계산단위가 일임. 5라고만적으면 5일전
+		Object[] data = {certDto.getCertEmail(), certDto.getCertNumber()};
+		List<CertDto> list = jdbcTemplate.query(sql, certMapper, data);
+		return list.isEmpty() ? false : true;
+		//return !list.isEmpty();
+	}
+
+}
+
+
+
+
+
+
+
