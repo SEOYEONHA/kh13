e88a5531b64e00fa119b4b8509906cd7ac7a0281
SEOYEONHA <only3322@naver.com>
2024-04-03 13:21:11 +0900

    가입환영 메세지

--- spring10integrated/src/main/java/com/kh/spring10/service/EmailService.java
+++ spring10integrated/src/main/java/com/kh/spring10/service/EmailService.java
@@ -1,17 +1,29 @@
 package com.kh.spring10.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Random;
+import java.util.Scanner;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kh.spring10.dao.CertDao;
 import com.kh.spring10.dao.MemberDao;
 import com.kh.spring10.dto.CertDto;
 import com.kh.spring10.dto.MemberDto;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -28,6 +40,52 @@ public class EmailService {
 		message.setTo(email);
 		message.setSubject("[KH정보교육원] 가입을 환영합니다");
 		message.setText("앞으로 많은 활동 부탁드립니다!");
+		
+		sender.send(message);
+	}
+	
+	public void sendWelcomeMail(MemberDto memberDto) throws IOException, MessagingException {
+		ClassPathResource resource = new ClassPathResource("templates/welcome-template.html");
+		File target = resource.getFile();
+		
+		StringBuffer buffer = new StringBuffer();
+		Scanner sc = new Scanner(target);
+		while(sc.hasNextLine()) {
+			buffer.append(sc.nextLine());
+		}
+		sc.close();
+		
+		Document document = Jsoup.parse(buffer.toString());
+		Element who = document.getElementById("who");
+		who.text(memberDto.getMemberNick());
+		
+		Element link = document.getElementById("login-link");
+		//link.attr("href", "로그인페이지주소");
+		//link.attr("href", "http://localhost:8080/member/login");//서버PC에서만
+		//link.attr("href", "http://192.168.30.200:8080/member/login");//강의장에서만(같은 인터넷 공유기 IP를 공유하는 곳에서만)
+		
+		//주소를 상황에 맞게 생성하는 도구 - ServletUriComponentsBuilder
+		//이메일에 우리 사이트 링크를 걸고싶을때 링크 전체를 적는것보다 이런식으로 자동으로 하는게 좋음
+		String url = ServletUriComponentsBuilder.fromCurrentContextPath()
+																	.path("/member/login")
+																	.build().toUriString();
+		
+		//url이라는 변수 안만들고 그냥 바로 넣어도 좋지만 가독성을 위해 한 것 ...
+		link.attr("href", url); 
+		
+		Element image = document.getElementById("background-img");
+		image.attr("src", ServletUriComponentsBuilder.fromCurrentContextPath()
+																.path("/image/mailTest.jpg")
+																.build().toUriString());
+		
+		
+		//마임메세지 생성
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+		
+		helper.setTo(memberDto.getMemberEmail());
+		helper.setSubject("[KH정보교육원] 가입을 환영합니다");
+		helper.setText(document.toString(), true);
 		
 		sender.send(message);
 	}
